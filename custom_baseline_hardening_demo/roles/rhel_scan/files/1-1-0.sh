83b9313696c04e3602a526a5e07e5dfb13d6d2ce
eanylin <anthony.jclin@gmail.com>
2019-11-16 19:18:56 +0800

    Update 1-1-0.sh

--- custom_baseline_hardening_demo/roles/rhel_scan/files/1-1-0.sh
+++ custom_baseline_hardening_demo/roles/rhel_scan/files/1-1-0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [[ $(grep -c -e "^server 10.10.10.10$" /etc/ntp.conf) -eq 1 && $(grep -c -e "^server 10.10.10.11$" /etc/ntp.conf) -eq 1 ]]; then
+if [[ $(grep -c -e "^server 10.10.10.10 iburst$" /etc/ntp.conf) -eq 1 && $(grep -c -e "^server 10.10.10.11 iburst$" /etc/ntp.conf) -eq 1 ]]; then
     exit 0
 else
     exit 1
